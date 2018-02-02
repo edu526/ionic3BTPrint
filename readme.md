ee4601bacd4fd1fb1a88605b633a6ea0cba31e86
Eduardo <edudelacruzrojas@gmail.com>
2018-02-02 11:17:05 -0500

    ne wplugin

--- readme.md
+++ readme.md
@@ -1,3 +1,8 @@
+# Change Plugin
+```
+$ cordova plugin add https://github.com/srehanuddin/Cordova-Plugin-Bluetooth-Printer.git --nofetch
+```
+
 # What is this?
 -This is an ionic 2+ app
 
@@ -12,7 +17,7 @@
 
 -Does not work on ionic serve, you have to compile the app and run it on a device
 
--Example: For pos system, for parking summons. 
+-Example: For pos system, for parking summons.
 
 
 
@@ -22,7 +27,7 @@
 2. Install ionic native bt serial. See this link:
 https://ionicframework.com/docs/native/bluetooth-serial/
 
-3. All the stuff are in the home.ts and in the providers/print/print.ts files 
+3. All the stuff are in the home.ts and in the providers/print/print.ts files
 
 -The files are quite self explanatory, but if you need any help, hit me up on twitter @razmans
 
@@ -39,3 +44,4 @@ https://ionicframework.com/docs/native/bluetooth-serial/
 Bitcoin: 1Bd2e8NjjGMTzULcFLQgQVFpAvUu15FJUC
 
 Paypal: http://paypal.me/razmansarit
+
--- src/providers/print/print.ts
+++ src/providers/print/print.ts
@@ -1,58 +1,126 @@
 import { Injectable } from '@angular/core';
-import {AlertController} from 'ionic-angular';
+import { AlertController } from 'ionic-angular';
 import { BluetoothSerial } from '@ionic-native/bluetooth-serial';
-
+import { Observable } from 'rxjs/Observable';
+declare var BTPrinter;
 @Injectable()
 export class PrintProvider {
 
-  constructor(private btSerial:BluetoothSerial,private alertCtrl:AlertController) {
-    
-  }
+    constructor(
+        // private btSerial: BluetoothSerial,
+        private alertCtrl: AlertController
+    ) {
 
-  searchBt()
-  {
-    return this.btSerial.list();
-  }
+    }
 
-  connectBT(address)
-  {
-    return this.btSerial.connect(address);
+    searchBt(): Promise<any[]> {
+        return new Promise((resolve, reject) => {
+            BTPrinter.list(function (data) {
+                data = data.map(dat => {
+                    return {
+                        name: dat,
+                        id: dat
+                    }
+                })
+                resolve(data);
+            }, function (err) {
+                reject(err);
+            })
+        })
+    }
 
-  }
+    connectBT(address): Observable<any> {
+        return Observable.create(observable => {
+            BTPrinter.connect(function (data) {
+                console.log("Success");
+                console.log(data)
 
-  testPrint(address)
-  {
-    let printData="Test hello this is a test \n\n\n\n Hello Test 123 123 123\n\n\n"
+                observable.next(data);
 
-    
-    let xyz=this.connectBT(address).subscribe(data=>{
-      this.btSerial.write(printData).then(dataz=>{
-        console.log("WRITE SUCCESS",dataz);
+            }, function (err) {
+                console.log("Error");
+                console.log(err)
 
-        let mno=this.alertCtrl.create({
-          title:"Print SUCCESS!",
-          buttons:['Dismiss']
+                observable.error(err);
+            }, address)
         });
-        mno.present();
 
-        xyz.unsubscribe();
-      },errx=>{
-        console.log("WRITE FAILED",errx);
-        let mno=this.alertCtrl.create({
-          title:"ERROR "+errx,
-          buttons:['Dismiss']
-        });
-        mno.present();
-      });
-      },err=>{
-        console.log("CONNECTION ERROR",err);
-        let mno=this.alertCtrl.create({
-          title:"ERROR "+err,
-          buttons:['Dismiss']
-        });
-        mno.present();
-      });
+        // return this.btSerial.connect(address);
+    }
 
-  }
+    disconnect(): Observable<any> {
+        return Observable.create(observable => {
+            BTPrinter.disconnect(function (data) {
+                console.log("Success");
+                console.log(data)
+                observable.next(data);
+            }, function (err) {
+                console.log("Error");
+                console.log(err)
+                observable.error(err);
+            })
+        })
+    }
+
+    testPrint(address) {
+        this.disconnect()
+            .subscribe(() => {
+                this._testPrint(address);
+            }, () => {
+                this._testPrint(address);
+            })
+    }
+
+    private _testPrint(address) {
+        let printData = "<h2>Test hello this is a test \n\n\n\n Hello Test 123 123 123\n\n\n<h2>"
+
+
+        let xyz = this.connectBT(address)
+            .subscribe(data => {
+                this._write(printData)
+                    .then(dataz => {
+                        console.log("WRITE SUCCESS", dataz);
+
+                        let mno = this.alertCtrl.create({
+                            title: "Print SUCCESS!",
+                            buttons: ['Dismiss']
+                        });
+                        mno.present();
+                        this.disconnect();
+                        xyz.unsubscribe();
+                    }, errx => {
+                        console.log("WRITE FAILED", errx);
+                        let mno = this.alertCtrl.create({
+                            title: "ERROR " + errx,
+                            buttons: ['Dismiss']
+                        });
+                        mno.present();
+                    });
+            }, err => {
+                console.log("CONNECTION ERROR", err);
+                let mno = this.alertCtrl.create({
+                    title: "ERROR " + err,
+                    buttons: ['Dismiss']
+                });
+                mno.present();
+            });
+
+    }
+
+    private _write(content) {
+        return new Promise((resolve, reject) => {
+            BTPrinter.printText(function (data) {
+                console.log("Success");
+                console.log(data)
+
+                resolve(data);
+            }, function (err) {
+                console.log("Error");
+                console.log(err)
+
+                reject(err);
+            }, content);
+        })
+    }
 
 }
